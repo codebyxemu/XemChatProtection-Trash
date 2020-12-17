0cb7733413c645135b5481bccf05994436173769
Code By Xemu <74688700+codebyxemu@users.noreply.github.com>
2020-12-17 10:19:46 +0100

    Art, Screenshots, Final Code
    For version BETA

--- src/main/java/me/xemu/xemchatprotection/XemChatProtection.java
+++ src/main/java/me/xemu/xemchatprotection/XemChatProtection.java
@@ -1,5 +1,7 @@
 package me.xemu.xemchatprotection;
 
+import me.xemu.xemchatprotection.events.PlayerChatEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class XemChatProtection extends JavaPlugin
@@ -10,6 +12,12 @@ public final class XemChatProtection extends JavaPlugin
     @Override public void onEnable()
     {
         plugin = this;
+
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+
+        Bukkit.getServer().getPluginManager().registerEvents(new PlayerChatEvent(), this);
+
         getLogger().info("Plugin Enabled");
     };
 
--- src/main/java/me/xemu/xemchatprotection/events/PlayerChatEvent.java
+++ src/main/java/me/xemu/xemchatprotection/events/PlayerChatEvent.java
@@ -1,10 +1,32 @@
 package me.xemu.xemchatprotection.events;
 
+import me.xemu.xemchatprotection.XemChatProtection;
+import me.xemu.xemchatprotection.utils.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class PlayerChatEvent implements Listener
 {
 
+    @EventHandler protected void onPlayerChat(AsyncPlayerChatEvent event)
+    {
 
+        final String message = event.getMessage().toLowerCase();
+        final Player messageAuthor = event.getPlayer();
+
+        for(String blocked : XemChatProtection.getPlugin().getConfig().getStringList("Words"))
+        {
+            if(message.contains(blocked.toLowerCase()))
+            {
+                event.setCancelled(true);
+                messageAuthor.sendMessage(Utils.chat(XemChatProtection.getPlugin().getConfig().getString("MessageBlocked").replaceAll("<message>", message).replaceAll("<player>", messageAuthor.getName())));
+                break;
+            }
+        }
+
+    };
 
 };
